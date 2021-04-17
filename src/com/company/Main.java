e2040d19c15373c5f866ffbdd77e43235b0a51ed
EmiZanotti <emizanotti032@gmail.com>
2021-04-17 17:35:01 -0300

    Ejecucion de hilos en Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,40 @@
 package com.company;
 
 import task.Biblioteca;
+import task.Escritor;
+import task.Lector;
 import task.Loggeador;
 
 public class Main {
 
     public static void main(String[] args) {
 	    Biblioteca biblioteca = new Biblioteca();
+        Thread[] escritores = new Thread[10];
+        Thread[] lectores = new Thread[20];
 	    boolean finish = false;
 	    Loggeador logger = new Loggeador();
 
+        //Inicializo los escritores
+        for (int i = 0; i < escritores.length; i++) {
+            escritores[i] = new Thread(new Escritor());
+            escritores[i].start();
+        }
+
+        //Inicializo los lectores
+        for (int i = 0; i < lectores.length; i++) {
+            lectores[i] = new Thread(new Lector());
+            lectores[i].start();
+        }
 
 	    while(!finish){
 
             logger.escribir(biblioteca.estadoActual());
             finish = true;
             for (int i = 0; i < 20; i++) {
-                finish = finish && (biblioteca.getLectores()[i].getState() == Thread.State.TERMINATED);
+                finish = finish && (lectores[i].getState() == Thread.State.TERMINATED);
             }
             for (int i = 0; i < 10; i++) {
-                finish = finish && (biblioteca.getEscritores()[i].getState() == Thread.State.TERMINATED);
+                finish = finish && (escritores[i].getState() == Thread.State.TERMINATED);
             }
             try {
                 Thread.currentThread().sleep(2000);
--- src/task/Biblioteca.java
+++ src/task/Biblioteca.java
@@ -4,26 +4,12 @@ package task;
 public class Biblioteca {
 
     private static Libro[] libros = new Libro[24]; //Utilizamos static pq estamos en un contexto estatico.
-    private Thread[] escritores = new Thread[10];
-    private Thread[] lectores = new Thread[20];
 
     public Biblioteca() {
 
         //Se crean los libros primero...
         for (int i=0; i< libros.length;i++){
             libros[i]=new Libro(i);
-        }
-
-        //Inicializo los escritores segundo
-        for (int i = 0; i < escritores.length; i++) {
-            escritores[i] = new Thread(new Escritor());
-            escritores[i].start();
-        }
-
-        //Inicializo los lectores
-        for (int i = 0; i < lectores.length; i++) {
-            lectores[i] = new Thread(new Lector());
-            lectores[i].start();
         }
 
     }
@@ -37,14 +23,6 @@ public class Biblioteca {
             }
         }
         return cont == 24;
-    }
-
-    public Thread[] getEscritores() {
-        return escritores;
-    }
-
-    public Thread[] getLectores() {
-        return lectores;
     }
 
     public static Libro[] getLibros() {
